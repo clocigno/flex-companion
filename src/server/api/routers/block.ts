549c9152934f80179de72ba785903e64dc9f47c2
Carl Locigno <clocigno@gmail.com>
2024-02-19 19:38:57 -0500

    Changes to Block schema and router

---
+++ src/server/api/routers/block.ts
@@ -0,0 +1,46 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  protectedProcedure,
+} from "~/server/api/trpc";
+
+export const blockRouter = createTRPCRouter({
+  create: protectedProcedure
+    .input(z.object({ 
+      pickupLocation: z.string().min(1), 
+      sceduledTimeStart: z.date(), 
+      sceduledTimeEnd: z.date(), 
+      pay: z.number().positive(), 
+      acceptedAt: z.date(), 
+      timeStart: z.date(),
+      timeEnd: z.date(),
+      milageStart: z.number().positive(),
+      milageEnd: z.number().positive(),
+      city: z.string().min(1),
+    }))
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.block.create({
+        data: {
+          pickupLocation: input.pickupLocation,
+          createdBy: { connect: { id: ctx.session.user.id } },
+          sceduledTimeStart: input.sceduledTimeStart,
+          sceduledTimeEnd: input.sceduledTimeEnd,
+          pay: input.pay,
+          acceptedAt: input.acceptedAt,
+          timeStart: input.timeStart,
+          timeEnd: input.timeEnd,
+          milageStart: input.milageStart,
+          milageEnd: input.milageEnd,
+          city: input.city,
+        },
+      });
+    }),
+
+  getLatest: protectedProcedure.query(({ ctx }) => {
+    return ctx.db.block.findMany({
+      orderBy: { createdAt: "desc" },
+      where: { createdBy: { id: ctx.session.user.id } },
+    });
+  }),
+});
